460e4b5d8fd2691dd881f3ce3ad3fce55a83d5f6
Mudith praveen <126862158+MudithPraveen@users.noreply.github.com>
2025-01-21 09:39:10 +0530

    Signin Validation

--- Screens/Login.js
+++ Screens/Login.js
@@ -1,13 +1,47 @@
-import { TextInput,Button,View,Text, StyleSheet } from 'react-native';
+import { Alert,TextInput,Button,View,Text, StyleSheet } from 'react-native';
+import {useState} from 'react';
 
 
 export default function Login({navigation}) {
+  const [username,setUn] = useState('');
+  const [password,setPw] = useState('');
+
+  function handleLogin(){
+    if(username && password){
+      if(password.length >= 6){
+        if(username === '2018ICTS75' && password === 'TS4068'){
+          navigation.navigate('Home');
+        }
+        else{
+          Alert.alert('Username or Password is incorrect!!');
+        }
+      }
+      else{
+        Alert.alert('Password must be at least 6 Characters!!');
+      }
+    }
+    else{
+      Alert.alert('Enter Username and Password!!');
+    }
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Login</Text>
-      <TextInput placeholder="User Name" style={styles.input}/>
-      <TextInput placeholder="Password" secureTextEntry style={styles.input}/>
-      <Button title='Sign In' onPress={() => navigation.navigate('Home')} />  
+      <TextInput 
+        placeholder="User Name" 
+        style={styles.input}
+        value={username}
+        onChangeText={setUn}
+      />
+      <TextInput 
+        placeholder="Password" 
+        secureTextEntry 
+        style={styles.input}
+        value={password}
+        onChangeText={setPw}
+      />
+      <Button title='Sign In' onPress={handleLogin} />  
       <Button color='lightblue' title="Go to Sign Up" onPress={() => navigation.navigate('Register')} />
     </View>
   );
